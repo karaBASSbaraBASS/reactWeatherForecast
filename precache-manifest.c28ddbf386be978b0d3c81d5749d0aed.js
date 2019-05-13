600dbe28ae5303393ea3f5f360994d2f72aa1748
Jack <evgen.mikhalov@gmail.com>
2019-05-13 03:49:25 +0300

    Updates

---
+++ precache-manifest.c28ddbf386be978b0d3c81d5749d0aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b43fa16153d4e0a7273f381d352bc70",
+    "url": "/reactWeatherForecast/index.html"
+  },
+  {
+    "revision": "def9cb442b42948d9320",
+    "url": "/reactWeatherForecast/static/css/main.24e5bebd.chunk.css"
+  },
+  {
+    "revision": "93e9c583d9402904f38a",
+    "url": "/reactWeatherForecast/static/js/2.9be3e040.chunk.js"
+  },
+  {
+    "revision": "def9cb442b42948d9320",
+    "url": "/reactWeatherForecast/static/js/main.040609cb.chunk.js"
+  },
+  {
+    "revision": "b20d620c6cb6740105c3",
+    "url": "/reactWeatherForecast/static/js/runtime~main.d99a6457.js"
+  }
+]);
